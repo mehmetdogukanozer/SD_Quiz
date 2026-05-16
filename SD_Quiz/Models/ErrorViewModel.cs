375a126667c8f45b537a3896551493d8cbd5214c
mehmetdogukanozer <mehmetdogukanozer2@gmail.com>
2026-05-16 22:36:41 +0300

    Proje dosyası ekle.

---
+++ SD_Quiz/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SD_Quiz.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
